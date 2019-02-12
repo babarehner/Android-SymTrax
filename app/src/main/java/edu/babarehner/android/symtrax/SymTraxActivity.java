c239b373eb6371f930188cbb7f129a946872d5dc
Mike Rehner <babarehner@gmail.com>
2019-02-12 11:55:35 -0500

    Subject: Set up layout for SymTrax/Main Activity
    What Happened: Refactored MainActivity filename to SymTrax file name in both java and xml files. Set up SymTraxActivity layout to include message to add records and to show a list view

--- app/src/main/java/edu/babarehner/android/symtrax/SymTraxActivity.java
+++ app/src/main/java/edu/babarehner/android/symtrax/SymTraxActivity.java
@@ -9,12 +9,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class SymTraxActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_symtrax);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
