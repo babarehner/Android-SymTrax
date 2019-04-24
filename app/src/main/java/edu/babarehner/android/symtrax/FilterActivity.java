04d582dbf41e7828340c88c85720657c2c60fcdf
Mike Rehner <babarehner@gmail.com>
2019-04-24 13:01:56 -0400

    Subject: Ciontinue Filter Implementaion
    What Happened: Added Symptom Filter with checkbx side by side with Emotion Filter

--- app/src/main/java/edu/babarehner/android/symtrax/FilterActivity.java
+++ app/src/main/java/edu/babarehner/android/symtrax/FilterActivity.java
@@ -1,21 +1,36 @@
 package edu.babarehner.android.symtrax;
 
+
+import android.content.ContentUris;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.content.Loader;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v4.app.LoaderManager;
+import android.app.LoaderManager;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import edu.babarehner.android.symtrax.data.SymTraxContract;
 
-public class FilterActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+import static edu.babarehner.android.symtrax.data.SymTraxContract.SymptomTableSchema.SYMPTOM_URI;
+import static edu.babarehner.android.symtrax.data.SymTraxContract.SymptomTableSchema._IDS;
 
+
+public class FilterActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>  {
+
+    private static final int FILTER_LOADER = 4;
+    private Uri mCurrentSymptomUri;
+    FilterSymptomCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +47,39 @@ public class FilterActivity extends AppCompatActivity implements LoaderManager.L
         listView.setAdapter(adapter);
 
 
+        ListView symptomListView = findViewById(R.id.right_list_view_symptom);
+
+        mCursorAdapter = new FilterSymptomCursorAdapter(this, null);
+        symptomListView.setAdapter(mCursorAdapter);
+
+        getLoaderManager().initLoader(FILTER_LOADER, null, this);
+
     }
 
     @NonNull
     @Override
     public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
-        return null;
+
+        String symptomSortOrder = SymTraxContract.SymptomTableSchema.C_SYMPTOM + " ASC";
+        mCurrentSymptomUri = SYMPTOM_URI;
+        String[] projectionSymptoms = {_IDS, SymTraxContract.SymptomTableSchema.C_SYMPTOM};
+        return new CursorLoader(this, mCurrentSymptomUri, projectionSymptoms, null,
+                null, symptomSortOrder);
     }
 
     @Override
-    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor c) {
+
+        // update(@link EquipTypeCursorAdapter) with this new cursor containing update Equpment Type data
+        mCursorAdapter.swapCursor(c);
 
     }
 
     @Override
     public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+        // Callback called when the data needs to be deleted- use null
+        mCursorAdapter.swapCursor(null);
 
     }
 }
--- app/src/main/java/edu/babarehner/android/symtrax/FilterSymptomCursorAdapter.java
+++ app/src/main/java/edu/babarehner/android/symtrax/FilterSymptomCursorAdapter.java
@@ -5,15 +5,16 @@ import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
 import static edu.babarehner.android.symtrax.data.SymTraxContract.SymptomTableSchema.C_SYMPTOM;
 
 
-public class FilterCursorAdapter extends CursorAdapter {
+public class FilterSymptomCursorAdapter extends CursorAdapter {
 
-    public FilterCursorAdapter(Context context, Cursor c) { super( context, c); }
+    public FilterSymptomCursorAdapter(Context context, Cursor c) { super( context, c); }
     /**
      * creates a new blank list item with no data
      * @param context   app context
@@ -24,7 +25,12 @@ public class FilterCursorAdapter extends CursorAdapter {
     @Override
     public View newView(Context context, Cursor c, ViewGroup parent) {
 
-        return LayoutInflater.from(context).inflate(R.layout.list_item_filter_emotions, parent, false);
+
+
+        return LayoutInflater.from(context).inflate(R.layout.list_item_filter_symptoms, parent, false);
+
+
+
     }
 
     /**
@@ -36,11 +42,12 @@ public class FilterCursorAdapter extends CursorAdapter {
     @Override
     public void bindView(View v, Context context, Cursor c) {
 
-        TextView emotionCheckBox = v.findViewById(R.id.list_item_filter_emotion_checkbox);
-        TextView emotionTextView = v.findViewById(R.id.list_item_filter_emotions);
+        //CheckBox cb = v.findViewById(R.id.list_item_filter_symptom_checkbox);
+        TextView symptomTextView = v.findViewById(R.id.list_item_symptoms);
         int symptomColIndex = c.getColumnIndex(C_SYMPTOM);
-        String emotion = c.getString(symptomColIndex);
-        emotionTextView.setText(emotion);
+        String symptom = c.getString(symptomColIndex);
+        //cb.setVisibility(v.VISIBLE);
+        symptomTextView.setText(symptom);
 
 
     }
