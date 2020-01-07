76c04bf63fb1fc89988247b07cd665ac99f79a7b
mike <babarehner@gmail.com>
2020-01-07 11:40:55 -0500

    Subject: Fix Merge Conflict
    What Happened: Accepted changes from 01.00.00_Release

--- app/src/main/java/edu/babarehner/android/symtrax/SymTraxActivity.java
+++ app/src/main/java/edu/babarehner/android/symtrax/SymTraxActivity.java
@@ -184,12 +184,13 @@ public class SymTraxActivity extends AppCompatActivity implements LoaderManager.
                 checkWritePermission();
                 bckupDB.writeCSVfile(cntxt);
                 return true;
-            /* removed because not implemented
+            /* Not implemented at this time
             case (R.id.action_set_filters):
                 Intent filterIntent = new Intent(SymTraxActivity.this, FilterActivity.class);
                 startActivity(filterIntent);
                 return true;
-            */
+
+             */
         }
         return super.onOptionsItemSelected(item);
     }
